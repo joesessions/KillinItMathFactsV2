4cd90a8117db11be4825450bedc7de8c0dd1623f
joesessions <36654608+joesessions@users.noreply.github.com>
2018-06-11 15:12:37 -0700

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KillinItMathFactsV2.Startup))]
+namespace KillinItMathFactsV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
